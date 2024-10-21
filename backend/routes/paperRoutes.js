ebd35499d28acfdff0518db691e5ac5741035bf1
sriram-iiith-24 <sriram.metla@students.iiit.ac.in>
2024-10-21 10:59:10 -1200

    Restructuring The Project Directories

--- backend/routes/paperRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/paperRoutes.js
-const express = require('express');
-const { searchPapers } = require('../controllers/paperController');
-
-const router = express.Router();
-
-router.get('/search', searchPapers);
-
-module.exports = router;
--- frontend/index.js
+++
@@ -1,7 +0,0 @@
-// frontend/src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css'; // Import styles
-
-ReactDOM.render(<App />, document.getElementById('root'));
